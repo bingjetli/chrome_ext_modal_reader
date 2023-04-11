b6bf30fb3d8a8b33fef0dcb949ec3fd2d4c0a2a1
bingjetli <bingjetli88@gmail.com>
2023-04-10 23:45:53 -0600

    fixed line break issues

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -70,7 +70,7 @@ const stripAttributes = html => {
 
 
 const insertSentenceLineBreaks = html => {
-    return html.replaceAll(/((?:[A-z0-9]+)(?:\s\S+){2,})([\.\?\!]+)/g, '$1$2<br>');
+    return html.replaceAll(/((?:[A-z0-9]+)(?:\s[^\.\?\!\s]+){2,})([\.\?\!]+)/g, '$1$2<br>');
 };
 
 const stripSvg = html => {
